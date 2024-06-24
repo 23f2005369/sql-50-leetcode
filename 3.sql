d81f601c9e317e458fcfbdf17e723540591ac723
23f2005369 <23f2005369@ds.study.iitm.ac.in>
2024-06-24 19:45:38 +0530

    h

--- 3.sql
+++ 3.sql
@@ -1,1 +1,1 @@
-SELECT name , population , area from World where area >= 300000 and population >= 25000000 ;
+SELECT name , population , area from World where area >= '3000000' or population >= '25000000' ;
